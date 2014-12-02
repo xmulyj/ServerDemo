9907bc6c7db391ba4ff4867d58e863dadd73045c
xmulyj <xmulyj@gmail.com>
2014-12-02 11:55:19 +0800

    Update CmdID.h
    add logic svr register cmd

--- common_define/CmdID.h
+++ common_define/CmdID.h
@@ -8,10 +8,15 @@
 #ifndef CMDID_H_
 #define CMDID_H_
 
+//LogicSvr
+#define CMD_REGISTER_REQ     2001  //注册
+#define CMD_REGISTER_RSP     2002
+
+
 //NameSvr
-#define CMD_ADD_NAME_REQ     5001
+#define CMD_ADD_NAME_REQ     5001  //添加名字
 #define CMD_ADD_NAME_RSP     5002
-#define CMD_DEL_NAME_REQ     5003
+#define CMD_DEL_NAME_REQ     5003  //删除名字
 #define CMD_DEL_NAME_RSP     5004
 
 
