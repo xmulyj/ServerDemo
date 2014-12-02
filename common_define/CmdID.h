d52ef637027c68300fb471e2116c830117d9e4bb
xmulyj <xmulyj@gmail.com>
2014-12-02 11:59:58 +0800

    Update CmdID.h

--- common_define/CmdID.h
+++ common_define/CmdID.h
@@ -8,16 +8,19 @@
 #ifndef CMDID_H_
 #define CMDID_H_
 
-//LogicSvr
+//SvrID=1, AccessSvr
+
+
+//SvrID=2, LogicSvr
 #define CMD_REGISTER_REQ     2001  //注册
 #define CMD_REGISTER_RSP     2002
 
 
-//NameSvr
-#define CMD_ADD_NAME_REQ     5001  //添加名字
-#define CMD_ADD_NAME_RSP     5002
-#define CMD_DEL_NAME_REQ     5003  //删除名字
-#define CMD_DEL_NAME_RSP     5004
+//SvrID=3, NameSvr
+#define CMD_ADD_NAME_REQ     3001  //添加名字
+#define CMD_ADD_NAME_RSP     3002
+#define CMD_DEL_NAME_REQ     3003  //删除名字
+#define CMD_DEL_NAME_RSP     3004
 
 
 
